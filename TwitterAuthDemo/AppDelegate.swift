1e9beb1d9272517a45f11e07dbd9684ead6e9293
Pol Quintana <pol.quintana1@gmail.com>
2016-08-27 14:22:33 +0200

    Fix Demo's AppDelegate 'openURL' method not being called

--- TwitterAuthDemo/AppDelegate.swift
+++ TwitterAuthDemo/AppDelegate.swift
@@ -46,7 +46,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
     }
 
-    func application(_ application: UIApplication, open url: URL, sourceApplication: String?, annotation: AnyObject) -> Bool {
+    func application(_ app: UIApplication, open url: URL, options: [UIApplicationOpenURLOptionsKey : Any] = [:]) -> Bool {
         TwitterAuth.sharedInstance.processAuthCallback(url)
         return false
     }
